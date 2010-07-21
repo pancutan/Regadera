72c99be2fd6ea1e79bd54a4b6b1a1c8156bb4157
Sergio Alonso <sergio@eim.esc.edu.ar>
2010-07-21 01:18:14 -0300

    listo para panadero

--- src/regadera/Main.java
+++ src/regadera/Main.java
@@ -21,16 +21,16 @@ Content-Transfer-Encoding: 7bit
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-  <meta content="text/html;charset=UTF-8" http-equiv="Content-Type">
+<meta content="text/html;charset=UTF-8" http-equiv="Content-Type">
 </head>
 <body>
- Escuela de Cocina para Niños y Adolescentes<br>
- Haga click <a href="http://www.eim.esc.edu.ar/site/descargas/peqyteens.jpg"">aqui</a> si no puede ver el siguiente mensaje.
+Escuela de Cocina para Niños y Adolescentes<br>
+Haga click <a href="http://www.eim.esc.edu.ar/site/descargas/peqyteens.jpg"">aqui</a> si no puede ver el siguiente mensaje.
 <img style="width: 550px; height: 2054px;" src="http://www.eim.esc.edu.ar/site/descargas/peqyteens.jpg"><br>
 ¡¡Agradecemos su difusión¡¡</p>
 <p></p>
 <p>Si desea desuscribirse, a nuestro boletin de noticias, haga click <a
- href="http://spreadsheets.google.com/viewform?hl=es&amp;formkey=dEpMQ0VOYnlPUmZQekNwTV9Tdk4zOFE6MA">aqui</a></p>
+href="http://spreadsheets.google.com/viewform?hl=es&amp;formkey=dEpMQ0VOYnlPUmZQekNwTV9Tdk4zOFE6MA">aqui</a></p>
 </body>
 </html>
  *
@@ -85,151 +85,154 @@ import java.util.Vector;
  */
 public class Main {
 
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) throws FileNotFoundException, IOException {
+
     /**
-     * @param args the command line arguments
+     * Comienza lectura de archivo
      */
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    File archivo = null;
+    FileReader fr = null;
+    BufferedReader br = null;
+
+    //    try {
+    // Apertura del fichero y creacion de BufferedReader para poder
+    // hacer una lectura comoda (disponer del metodo readLine()).
+    archivo = new File("regadera/gloria5.txt");
+    fr = new FileReader(archivo);
+    br = new BufferedReader(fr);
+
+    // Lectura del fichero
+    String linea;
+    Vector v = new Vector();
+
+    while ((linea = br.readLine()) != null) {
+      System.out.println("Alimentando un Vector v con: " + linea);
+      v.addElement(linea);
+    }
+
+    try {
+
+      //anda esto? configurar ssmtp.conf
+      //y disparar en consola algo tipo
+      //ssmtp -v escuelaint@gmail.com < regadera/htmls/panadero.html
+
+      //Me envio una copia...
+      //String[] command = {"sh", "-c", "ssmtp -v escuelaint@gmail.com < regadera/htmls/panadero.html"};
+
+
+      for (int i = 0; i < v.size(); i++) {
+        // aqui sacamos el objeto que se encuentra en el indice o posicion i-esima
+        Object objeto = v.elementAt(i);
+
+        // si queremos convertirlo a algun tipo, nos toca hacer un casting (Tipo), asi:
+        // ojo! esto solo lo puedes hacer si agregaste objetos de tipo String en el vector
+        String str = (String) objeto;
+        //System.out.println("Leyendo desde vector: " + str);
 
         /**
-         * Comienza lectura de archivo
+         * Comienza ejecución de comando en consola
          */
-        File archivo = null;
-        FileReader fr = null;
-        BufferedReader br = null;
+        try {
+          //String command = "ls -lh ";
 
-        //    try {
-        // Apertura del fichero y creacion de BufferedReader para poder
-        // hacer una lectura comoda (disponer del metodo readLine()).
-        archivo = new File("regadera/limpios4.txt");
-        fr = new FileReader(archivo);
-        br = new BufferedReader(fr);
+          String[] command = {"sh", "-c", "ssmtp -v " + str + " < regadera/htmls/panadero.html"};
 
-        // Lectura del fichero
-        String linea;
-        Vector v = new Vector();
+          //guardo en el log lo que guardé...
+          try {
+            BufferedWriter out = new BufferedWriter(new FileWriter("regadera/traza.log", true));
+            out.write(str + "\n");
+            out.close();
+          } catch (IOException e) {
+          }
 
-        while ((linea = br.readLine()) != null) {
-            System.out.println("Alimentando un Vector v con: " + linea);
-            v.addElement(linea);
+          //corro el proceso
+          final Process process = Runtime.getRuntime().exec(command);
+          new Thread() {
+
+            @Override
+            public void run() {
+              try {
+                InputStream is = process.getInputStream();
+                byte[] buffer = new byte[1024];
+                for (int count = 0; (count = is.read(buffer)) >= 0;) {
+                  System.out.write(buffer, 0, count);
+                }
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+            }
+          }.start();
+          new Thread() {
+
+            public void run() {
+              try {
+                InputStream is = process.getErrorStream();
+                byte[] buffer = new byte[1024];
+                for (int count = 0; (count = is.read(buffer)) >= 0;) {
+                  System.err.write(buffer, 0, count);
+                }
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+            }
+          }.start();
+
+          int returnCode = process.waitFor();
+          System.out.println("Return code = " + returnCode);
+
+          System.out.println("Enviado mail: " + str + " " + (i + 1));//para que no empiece a contar desde el 0
+
+
+
+
+        } catch (Exception e) {
+          e.printStackTrace();
         }
 
         try {
+          System.out.println("Reteniendo siguiente mensaje...");
+          //Thread.sleep(30 * 1000);
+          Thread.sleep(60 * 1000);
 
-            //Me envio una copia...
-            //String[] command = {"sh", "-c", "ssmtp -v escuelaint@gmail.com < regadera/htmls/celiaco.html"};
-
-
-            for (int i = 0; i < v.size(); i++) {
-                // aqui sacamos el objeto que se encuentra en el indice o posicion i-esima
-                Object objeto = v.elementAt(i);
-
-                // si queremos convertirlo a algun tipo, nos toca hacer un casting (Tipo), asi:
-                // ojo! esto solo lo puedes hacer si agregaste objetos de tipo String en el vector
-                String str = (String) objeto;
-                //System.out.println("Leyendo desde vector: " + str);
-
-                /**
-                 * Comienza ejecución de comando en consola
-                 */
-                try {
-                    //String command = "ls -lh ";
-
-                    String[] command = {"sh", "-c", "ssmtp -v " + str + " < regadera/htmls/pagweb.html"};
-
-                    //guardo en el log lo que guardé...
-                    try {
-                        BufferedWriter out = new BufferedWriter(new FileWriter("regadera/traza.log", true));
-                        out.write(str + "\n");
-                        out.close();
-                    } catch (IOException e) {
-                    }
-
-                    //corro el proceso
-                    final Process process = Runtime.getRuntime().exec(command);
-                    new Thread() {
-
-                        @Override
-                        public void run() {
-                            try {
-                                InputStream is = process.getInputStream();
-                                byte[] buffer = new byte[1024];
-                                for (int count = 0; (count = is.read(buffer)) >= 0;) {
-                                    System.out.write(buffer, 0, count);
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }.start();
-                    new Thread() {
-
-                        public void run() {
-                            try {
-                                InputStream is = process.getErrorStream();
-                                byte[] buffer = new byte[1024];
-                                for (int count = 0; (count = is.read(buffer)) >= 0;) {
-                                    System.err.write(buffer, 0, count);
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }.start();
-
-                    int returnCode = process.waitFor();
-                    System.out.println("Return code = " + returnCode);
-
-                    System.out.println("Enviado mail: " + str + " " + (i + 1));//para que no empiece a contar desde el 0
+        } catch (Exception ex) {
+          ex.printStackTrace();
+        }
 
 
 
+      }//fin for
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    System.out.println("Reteniendo siguiente mensaje...");
-                    //Thread.sleep(30 * 1000);
-                    Thread.sleep(60 * 1000);
-
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-
-
-
-            }//fin for
-
-            //escribir.close();
+      //escribir.close();
 //            } catch (EOFException e) { //fin escritura de archivo
 //
 //                System.out.println("Final de Stream");
 //            }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // En el finally cerramos el fichero, para asegurarnos
-            // que se cierra tanto si todo va bien como si salta
-            // una excepcion.
-            try {
-                if (null != fr) {
-                    fr.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
-
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      // En el finally cerramos el fichero, para asegurarnos
+      // que se cierra tanto si todo va bien como si salta
+      // una excepcion.
+      try {
+        if (null != fr) {
+          fr.close();
         }
+      } catch (Exception e2) {
+        e2.printStackTrace();
+      }
 
-        System.out.println("Programa terminado");
+    }
+
+    System.out.println("Programa terminado");
 
 
 
 
 
-    }//fin public static void main(String[] args) {
+  }//fin public static void main(String[] args) {
 }//Fin public class Main {
-
 
